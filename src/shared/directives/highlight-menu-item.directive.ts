ed19cd71e187098c6618810fe5b5a36c4d6e0516
Roman Chuykov <chuykov95@mail.ru>
2024-04-23 18:19:14 +0300

    правки

--- src/shared/directives/highlight-menu-item.directive.ts
+++ src/shared/directives/highlight-menu-item.directive.ts
@@ -1,63 +1,87 @@
-import { Directive, HostListener, ElementRef, OnInit, AfterViewInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+	Directive,
+	HostListener,
+	ElementRef,
+	OnInit,
+	AfterViewInit,
+	PLATFORM_ID,
+	Inject,
+} from '@angular/core';
 
 @Directive({
 	selector: '[appHighlightMenuItem]',
 	standalone: true,
 })
 export class HighlightMenuItemDirective implements AfterViewInit {
+	private isBrowser: boolean;
+
+	constructor(
+		private el: ElementRef<HTMLElement>,
+		@Inject(PLATFORM_ID) private platformId: Object
+	) {
+		this.isBrowser = isPlatformBrowser(this.platformId);
+	}
+
 	ngAfterViewInit(): void {
 		this.onWindowScroll();
 	}
 
 	@HostListener('window:scroll', [])
 	onWindowScroll() {
-		const currentSectionId = this.getCurrentSectionId();
-		const menuItems = document.querySelectorAll('.menu-item');
+		if (this.isBrowser) {
+			const currentSectionId = this.getCurrentSectionId();
+			const menuItems = document.querySelectorAll('.menu-item');
 
-		menuItems.forEach((item) => {
-			const itemId = item.getAttribute('data-section-id');
+			menuItems.forEach((item) => {
+				const itemId = item.getAttribute('data-section-id');
 
-			if (itemId === currentSectionId) {
-				item.classList.add('active');
-			} else {
-				item.classList.remove('active');
-			}
-		});
+				if (itemId === currentSectionId) {
+					item.classList.add('active');
+				} else {
+					item.classList.remove('active');
+				}
+			});
+		}
 	}
 
 	getCurrentSectionId(): string {
-		const scrollPositionTop =
-			window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-		const scrollPositionBottom = scrollPositionTop + window.innerHeight;
+		if (this.isBrowser) {
+			const scrollPositionTop =
+				window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+			const scrollPositionBottom = scrollPositionTop + window.innerHeight;
 
-		const sections = document.querySelectorAll('.section') as NodeListOf<HTMLElement>;
+			const sections = document.querySelectorAll('.section') as NodeListOf<HTMLElement>;
 
-		let currentSectionId = '';
-		let currentSectionIsFullView = false;
+			let currentSectionId = '';
+			let currentSectionIsFullView = false;
 
-		sections.forEach((section) => {
-			const sectionToTop = section.offsetTop;
-			const sectionHeight = section.offsetHeight;
-			const sectionToBottom = sectionToTop + sectionHeight;
+			sections.forEach((section) => {
+				const sectionToTop = section.offsetTop;
+				const sectionHeight = section.offsetHeight;
+				const sectionToBottom = sectionToTop + sectionHeight;
 
-			/** Определяет видно ли секцию на странице хотя бы на половину */
-			const isSectionHalfInView = scrollPositionBottom > sectionToTop + (sectionHeight / 5) * 3.5;
+				/** Определяет видно ли секцию на странице хотя бы на половину */
+				const isSectionHalfInView = scrollPositionBottom > sectionToTop + (sectionHeight / 5) * 3.5;
 
-			/** Определяет полностью ли видно секцию на странице */
-			const isSectionFullyInView =
-				scrollPositionTop < sectionToTop && scrollPositionBottom > sectionToBottom - 10;
+				/** Определяет полностью ли видно секцию на странице */
+				const isSectionFullyInView =
+					scrollPositionTop < sectionToTop && scrollPositionBottom > sectionToBottom - 10;
 
-			/** Если есть блок который видно во всю страницу, и найденный не во всю страницу */
-			if (currentSectionIsFullView && !isSectionFullyInView) {
-				return;
-			}
+				/** Если есть блок который видно во всю страницу, и найденный не во всю страницу */
+				if (currentSectionIsFullView && !isSectionFullyInView) {
+					return;
+				}
 
-			if (isSectionHalfInView || isSectionFullyInView) {
-				currentSectionId = section.getAttribute('id')!;
-				currentSectionIsFullView = isSectionFullyInView;
-			}
-		});
+				if (isSectionHalfInView || isSectionFullyInView) {
+					currentSectionId = section.getAttribute('id')!;
+					currentSectionIsFullView = isSectionFullyInView;
+				}
+			});
 
-		return currentSectionId;
+			return currentSectionId;
+		} else {
+			return '';
+		}
 	}
 }
