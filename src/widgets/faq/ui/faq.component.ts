58375a70fa789ec2a3ebe38e4e7be41b359e455e
gaspromov <gaspromov@mail.ru>
2024-04-24 18:58:50 +0300

    [new] Fix font of description text + refactoring

--- src/widgets/faq/ui/faq.component.ts
+++ src/widgets/faq/ui/faq.component.ts
@@ -1,37 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
 	selector: 'app-faq',
 	standalone: true,
-	imports: [CommonModule, MatButtonModule, MatIconModule, MatExpansionModule],
+	imports: [CommonModule, MatIconModule, MatExpansionModule],
 	templateUrl: './faq.component.html',
 	styleUrl: './faq.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	encapsulation: ViewEncapsulation.None,
-	animations: [
-		trigger('fadeOut', [
-			state(
-				'visible',
-				style({
-					opacity: 1,
-				})
-			),
-			state(
-				'hidden',
-				style({
-					opacity: 0,
-					display: 'none', // Добавляем display: none для скрытия элемента
-				})
-			),
-			transition('visible => hidden', animate('0.5s ease-out')),
-			transition('hidden => visible', animate('0.5s ease-out')),
-		]),
-	],
 })
 export class FaqComponent {
 	expConfig = [
