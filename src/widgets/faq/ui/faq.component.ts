a1422a63fb95ccd2933ac545ee5728ec888e6a47
gaspromov <gaspromov@mail.ru>
2024-04-24 23:13:49 +0300

    [minor]

--- src/widgets/faq/ui/faq.component.ts
+++ src/widgets/faq/ui/faq.component.ts
@@ -23,19 +23,19 @@ export class FaqComponent {
 		{
 			title: 'How can i receive mails from accounts?',
 			description:
-				'Yes, you get full access to the account, both to the email and to the account itself. We also provide access to all security measures, including 2FA code.',
+				'All emails from the emails will be forwarded to your email or domain, which we will request from you. However, you will also have direct access to each email.',
 			open: false,
 		},
 		{
 			title: 'Whats difference between history and aged?',
 			description:
-				'Yes, you get full access to the account, both to the email and to the account itself. We also provide access to all security measures, including 2FA code.',
+				'The only difference lies in the presence of order history. Accounts with order history already have ticket purchases. The number of tickets is random. It is believed that such accounts occupy better positions in the queue compared to regular aged accounts. And yes, accounts with history are also aged.',
 			open: false,
 		},
 		{
 			title: 'Do you have bulk discounts?',
 			description:
-				'Yes, you get full access to the account, both to the email and to the account itself. We also provide access to all security measures, including 2FA code.',
+				'Yes, of course, we offer pleasant discounts for large volume orders. Additionally, we collaborate directly with ticket brokers and provide them with individual terms.',
 			open: false,
 		},
 	];
