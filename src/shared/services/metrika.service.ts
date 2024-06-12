097ea5e9a7d974281584749de66d2f8e840c1011
gaspromov <gaspromov@mail.ru>
2024-06-12 21:14:07 +0300

    [minor] Minor handler for ReachGoals

--- src/shared/services/metrika.service.ts
+++ src/shared/services/metrika.service.ts
@@ -13,8 +13,12 @@ export class MetrikaService {
 	private readonly Y_ID = 97142453;
 
 	reachGoal(goals: GoalParams) {
-		(window as any).ym(this.Y_ID, 'reachGoal', goals.ym);
-		(window as any).twq('event', goals.tw);
-		(window as any).fbq('track', goals.meta);
+		try {
+			(window as any).ym(this.Y_ID, 'reachGoal', goals.ym);
+			(window as any).twq('event', goals.tw);
+			(window as any).fbq('track', goals.meta);
+		} catch (e) {
+			console.log(e);
+		}
 	}
 }
